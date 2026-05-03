f44aca5388c827d19b54469cb6e2270a1b917c6e
natasya <n341566@gmail.com>
2026-05-03 16:12:31 +0700

    update list arsip pimdin

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -182,6 +182,25 @@ $arsip = $query->latest()->get()->map(function ($item) {
 
     return $item;
 });
+
+    //hmch coba
+    $user = Auth::user();
+
+    if ($user->role === 'pimpinan') {
+        return Inertia::render('Pimpinan/ListArsipPimpinan', [
+            'arsip' => $arsip,
+            'kategori' => Kategori::with('childrenRecursive')
+                ->whereNull('parent_id')
+                ->get(),
+            'filters' => $request->only([
+                'search',
+                'kategori',
+                'tanggal_awal',
+                'tanggal_akhir'
+            ])
+        ]);
+    }
+
     return Inertia::render('ListArsip', [
         'arsip' => $arsip,
         'kategori' => Kategori::with('childrenRecursive')
