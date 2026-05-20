e57c0c1c06a0eda26c592d1d1ee9673d0f4b1a32
natasya <n341566@gmail.com>
2026-05-20 13:39:05 +0700

    UPDATE BGT NI YACH

--- resources/js/pages/Pimpinan/StatistikPimpinan.vue
+++ resources/js/pages/Pimpinan/StatistikPimpinan.vue
@@ -266,7 +266,7 @@ const barChartOptions = {
 
   </div>
 
-  <!-- TABLE USER -->
+  <!-- TABEL USER -->
   <div class="overflow-hidden rounded-[2rem] shadow-xl border border-gray-100 space-y-4">
 
     <h2 class="bg-[#2f4fa2] text-white px-4 py-2 rounded-lg w-fit text-sm font-semibold mb-2">
